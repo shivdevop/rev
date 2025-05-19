9911140d7bf2654d687e6c456decb1d93f995302
shivam <shivamagarwalla05@gmail.com>
2025-05-20 04:09:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express=require("express")
+const app=express()
+
+require("dotenv").config()
+PORT=process.env.PORT || 3000
+
+// for body parsing 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+// create an inmemory array of user objects
+users=[
+    {
+        name:"shivam",
+        kidneys:[
+            {healthy:true},
+            {healthy:false}
+        ]
+    },
+    {
+        name:"John",
+        kidneys:[
+            {healthy:true},
+            {healthy:true}
+        ]
+    }
+]
+
+//four functionalities we need to implement
+
+
+
+//user can check how many kidneys they have and their health status
+app.get("/",(req,res)=>{
+    let JohnKidneys=users[0].kidneys
+    let numberOfKidneys=JohnKidneys.length
+    let healthyKidneys=JohnKidneys.filter(kidney=>kidney.healthy)
+    let healthyKidneysCount=healthyKidneys.length
+    let unhealthyKidneysCount=numberOfKidneys-healthyKidneysCount
+    res.json({
+        numberOfKidneys:numberOfKidneys,
+        healthyKidneysCount:healthyKidneysCount,
+        unhealthyKidneysCount:unhealthyKidneysCount
+    })
+})
+
+//user can add a new kidney, it can be healthy or unhealthy
+app.post("/",(req,res)=>{
+    let newKidneyStatus=req.body.healthy
+    let newKidney={ healthy:newKidneyStatus }
+    users[0].kidneys.push(newKidney)
+    res.json({
+        msg:"success"
+    })
+})
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
+})
